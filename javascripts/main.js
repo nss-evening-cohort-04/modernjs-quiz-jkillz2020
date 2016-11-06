df6ff7a87438769dcae26a8fca85704fb2fb1916
jkillz2020 <kilgore.josh@gmail.com>
2016-11-05 20:07:32 -0500

    everything working except battle damage logic

--- javascripts/main.js
+++ javascripts/main.js
@@ -162,6 +162,7 @@ function putWeaponsToDom(){
           weaponPicker+='<option>Lazer</option>';
           weaponPicker+='<option>Buzzsaw</option>';
           weaponPicker+='</select>';
+          weaponPicker+='<button class="hide" id="player-one-weapon-btn">Select Weapon</button>';
     $('#player-one-bot-select').append(weaponPicker);
   }
   putWeaponsToDom();
@@ -174,6 +175,7 @@ function putWeaponsToDom(){
           weaponPicker+='<option>Lazer</option>';
           weaponPicker+='<option>Buzzsaw</option>';
           weaponPicker+='</select>';
+          weaponPicker+='<button class="hide" id="player-two-weapon-btn">Select Weapon</button>';
     $('#player-two-bot-select').append(weaponPicker);
   }
   putWeaponsToDom2();
@@ -184,113 +186,149 @@ function putWeaponsToDom(){
 
 
   //Players select robot//
+  //PLAYER ONE BOT SELECT//
+  //
+  //
   $('#player-one-bot-type').on('click', function(){
     if ($('.selectpicker',$(this).parent()).val() === null) {
-      alert("Please select a Robot!");
+      window.alert("Please select a Robot!");
       return;
     }
     if ($("#player-one-name").val() === "" ) {
-      alert("Please enter a name!");
+      window.alert("Please enter a name!");
       return;
     }
-    console.log($('.selectpicker',$(this).parent()).val());
-    
-  let PlayerOne = $("#player-one-name").val();
-    if(
-      $('.selectpicker',$(this).parent()).val() === "Raptor"){
-         Battledome.PlayerOne =  {
-          name: PlayerOne,
-          robot:new Raptor(),
-          health: this.totalLife
-        };
-    }
-    else if(
-      $('.selectpicker',$(this).parent()).val() === "Bigfoot"){
-         Battledome.PlayerOne =  {
-          name: PlayerOne,
-          robot:new BigFoot(),
-          health: this.totalLife
-        };
+      let PlayerOneName = $("#player-one-name").val();
+    if($('.selectpicker',$(this).parent()).val() === "Raptor"){
+        Battledome.PlayerOne.Robot = new Raptor();
       }
-    else if(
-      $('.selectpicker',$(this).parent()).val() === "Hulk"){
-         Battledome.PlayerOne =  {
-          name: PlayerOne,
-          robot:new Hulk(),
-          health: this.totalLife
-        };
+    else if($('.selectpicker',$(this).parent()).val() === "Bigfoot"){
+         Battledome.PlayerOne.Robot = new BigFoot();
       }
-    else if(
-      $('.selectpicker',$(this).parent()).val() === "Tank Drone"){
-         Battledome.PlayerOne =  {
-          name: PlayerOne,
-          robot:new TankDrone(),
-          health: this.totalLife
-        };
+    else if($('.selectpicker',$(this).parent()).val() === "Hulk"){
+      Battledome.PlayerOne.Robot = new Hulk();
       }
-    else if(
-      $('.selectpicker',$(this).parent()).val() === "Predator Drone"){
-         Battledome.PlayerOne =  {
-          name: PlayerOne,
-          robot:new PredatorDrone(),
-          health: this.totalLife
-        };
+    else if($('.selectpicker',$(this).parent()).val() === "Tank Drone"){
+      Battledome.PlayerOne.Robot = new TankDrone();   
       }
-    else if(
-      $('.selectpicker',$(this).parent()).val() === "Jet Drone"){
-         Battledome.PlayerOne =  {
-          name: PlayerOne,
-          robot:new JetDrone(),
-          health: this.totalLife
-        };
+    else if($('.selectpicker',$(this).parent()).val() === "Predator Drone"){
+      Battledome.PlayerOne.Robot = new PredatorDrone();
+      }
+    else if($('.selectpicker',$(this).parent()).val() === "Jet Drone"){
+      Battledome.PlayerOne.Robot = new JetDrone();
   }
+  Battledome.PlayerOne.name = PlayerOneName;
   $('.selectpicker',$(this).parent()).addClass("hide");
   $('#player-one-weapon-picker').removeClass("hide");
+  $('#player-one-weapon-btn').removeClass("hide");
   $('#player-one-battle-area').append($('.selectpicker',$(this).parent()).val());
+  console.log("playerOne",Battledome.PlayerOne);
   });
 
+//PLAYER TWO BOT SELECT//
+//
   $('#player-two-bot-type').on('click', function(){
+    if ($('.selectpicker',$(this).parent()).val() === null) {
+      window.alert("Please select a Robot!");
+      return;
+    }
+    if ($("#player-two-name").val() === "" ) {
+      window.alert("Please enter a name!");
+      return;
+    }
     let playerTwoName = $("#player-two-name").val();
     if($('.selectpicker',$(this).parent()).val() === "Raptor"){
-      Battledome.PlayerTwo =  new Raptor();
+      Battledome.PlayerTwo.Robot =  new Raptor();
     }
     else if($('.selectpicker',$(this).parent()).val() === "Bigfoot"){
-      Battledome.PlayerTwo = new BigFoot();
+      Battledome.PlayerTwo.Robot = new BigFoot();
     }
     else if($('.selectpicker',$(this).parent()).val() === "Hulk"){
-      Battledome.PlayerTwo = new Hulk();
+      Battledome.PlayerTwo.Robot = new Hulk();
     }
     else if($('.selectpicker',$(this).parent()).val() === "Tank Drone"){
-      Battledome.PlayerTwo =  new TankDrone();
+      Battledome.PlayerTwo.Robot =  new TankDrone();
     }
     else if($('.selectpicker',$(this).parent()).val() === "Predator Drone"){
-      Battledome.PlayerTwo =  new PredatorDrone();
+      Battledome.PlayerTwo.Robot =  new PredatorDrone();
     }
     else if($('.selectpicker',$(this).parent()).val() === "Jet Drone"){
-      Battledome.PlayerTwo =  new JetDrone();
+      Battledome.PlayerTwo.Robot =  new JetDrone();
     }
 
     Battledome.PlayerTwo.name = playerTwoName;
     $('.selectpicker',$(this).parent()).addClass("hide");
     $('#player-two-weapon-picker').removeClass("hide");
+      $('#player-two-weapon-btn').removeClass("hide");
     $('#player-two-battle-area').append($('.selectpicker',$(this).parent()).val());
     console.log("playerTwo",Battledome.PlayerTwo);
   });
+//PLAYERS PICK WEAPON//
+//
+//PLAYER ONE WEAPON SELECT///
+  $('#player-one-weapon-btn').on('click', function(){
+      if ($('#player-one-weapon-picker',$(this).parent()).val() === null) {
+        alert("Please select a Weapon!");
+        return;
+      }
+      if($('#player-one-weapon-picker',$(this).parent()).val() === "Gun"){
+        Battledome.PlayerOne.Weapon =  new Gun();
+      }
+      else if($('#player-one-weapon-picker',$(this).parent()).val() === "Bubbles"){
+        Battledome.PlayerOne.Weapon = new Bubbles();
+      }
+      else if($('#player-one-weapon-picker',$(this).parent()).val() === "Thor Hammer"){
+        Battledome.PlayerOne.Weapon = new ThorHammer();
+      }
+      else if($('#player-one-weapon-picker',$(this).parent()).val() === "Lazer"){
+        Battledome.PlayerOne.Weapon =  new Lazer();
+      }
+      else if($('#player-one-weapon-picker',$(this).parent()).val() === "Buzzsaw"){
+        Battledome.PlayerOne.Weapon =  new Buzzsaw();
+      }
 
+    $('#player-one-battle-area').append(" with a " + $('#player-one-weapon-picker',$(this).parent()).val());
+    });
+
+  //PLAYER TWO WEAPON SELECT//
+  
+  $('#player-two-weapon-btn').on('click', function(){
+      if ($('#player-two-weapon-picker',$(this).parent()).val() === null) {
+        alert("Please select a Weapon!");
+        return;
+      }
+      if($('#player-two-weapon-picker',$(this).parent()).val() === "Gun"){
+        Battledome.PlayerTwo.Weapon =  new Gun();
+      }
+      else if($('#player-two-weapon-picker',$(this).parent()).val() === "Bubbles"){
+        Battledome.PlayerTwo.Weapon = new Bubbles();
+      }
+      else if($('#player-two-weapon-picker',$(this).parent()).val() === "Thor Hammer"){
+        Battledome.PlayerTwo.Weapon = new ThorHammer();
+      }
+      else if($('#player-two-weapon-picker',$(this).parent()).val() === "Lazer"){
+        Battledome.PlayerTwo.Weapon =  new Lazer();
+      }
+      else if($('#player-two-weapon-picker',$(this).parent()).val() === "Buzzsaw"){
+        Battledome.PlayerTwo.Weapon =  new Buzzsaw();
+      }
+
+    $('#player-two-battle-area').append(" with a " + $('#player-two-weapon-picker',$(this).parent()).val());
+    });
 
   $("#attack-button").on('click', function(){
-    PlayerOne.health = (Battledome.PlayerOne.health - Battledome.PlayerTwo.weapon.damage);
-        if (PlayerOne.health - PlayerTwo.weapon.damage <= 0) {
-                playerOneDied();
+    Battledome.PlayerOne.health = (Battledome.PlayerOne.Robot.totalLife - Battledome.PlayerTwo.Weapon.damage);
+        if (Battledome.PlayerOne.health - Battledome.PlayerTwo.weapon.damage <= 0) {
+                alert("Player One Died!");
         }
-        $("#player-one-health").html("Health " + (PlayerOne.health - PlayerTwo.weapon.damage));
+        //$("#player-one-health").html("Health " + (PlayerOne.health - PlayerTwo.weapon.damage));
       //Player Two//
-        PlayerTwo.health = (Battledome.PlayerTwo.health - Battledome.PlayerOne.weapon.damage);
-        if (PlayerTwo.health - PlayerOne.weapon.damage <= 0) {
-                PlayerTwoDied();
+        Battledome.playerTwo.health = (Battledome.PlayerTwo.Robot.totalLife - Battledome.PlayerOne.weapon.damage);
+        if (Battledome.playerTwo.health - Battledome.PlayerOne.weapon.damage <= 0) {
+                alert("Player Two Died!");
         }
-        $("#player-two-health").html("Health " + (PlayerTwo.health - PlayerOne.weapon.damage));
-   });
+        // $("#player-two-health").html("Health " + (PlayerTwo.health - PlayerOne.weapon.damage));
+    });
 });
 
 
